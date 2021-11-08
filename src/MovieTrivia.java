5c1dac675fcc02db79f2e65dabe3449f2755472b
Luna <flunawonder@gmail.com>
2021-11-08 13:16:41 -0500

    finished

--- src/MovieTrivia.java
+++ src/MovieTrivia.java
@@ -55,5 +55,235 @@ public class MovieTrivia {
 	
 	// TODO add additional methods as specified in the instructions PDF
 	
-	
+    /**
+     * Insert given actor and his/her movies into database. 
+     * If the given actor is not already present in the given actorsInfo arraylist, 
+     * this method should append the given actor along with their given movies to the end of actorsInfo.
+     * If the given actor is already present in the given actorsInfo arraylist, it should append
+     * the given movies to the end of the actor object's movieCasted list.
+     * Note: Actor names should not be case sensitive, but spelling must be correct.
+     */
+	public void insertActor (String actor, String [] movies, ArrayList <Actor> actorsInfo){
+        
+        //insert or find the actor 
+        String actorName = actor.trim().toLowerCase();   // make sure the name is not case-sensitivie + trim white space       
+        Boolean exsit = false;  
+        
+        for (Actor targetActor:actorsInfo){
+            if ( targetActor.getName().toLowerCase().trim().equals(actorName)){ // == is wrong
+                for (String movie : movies){
+                     targetActor.getMoviesCast().add(movie.trim().toLowerCase()); 
+        //question about editing ArrayList by reference: 
+        //am I updating the private ArrayList <String> moviesCasted ?? or am I editing what is shown by actor.getMoviesCast()?  
+        //actor.setMoviesCast(actorMovies)    
+                }
+                return; //case 1
+            }
+        }
+    
+        Actor targetActor = new Actor(actorName); 
+        //ArrayList<String> actorMovies = new ArrayList<String>(); //case2
+        //targetActor.getMoviesCast() = actorMovies; //shouldn't we be editing .moviesCasted?
+        for (String movie : movies){
+                targetActor.getMoviesCast().add(movie.trim().toLowerCase()); 
+                }
+        actorsInfo.add(targetActor); //insert the actor if not already there
+        return ;
+      }
+    
+    /**
+     * Insert given ratings for given movie into database.
+     *  Ratings is an int array with 2 elements: the critics’ rating at index 0 and the audience rating at index 1.
+     */
+    public void insertRating (String movie, int [] ratings, ArrayList <Movie> moviesInfo){
+        String newMovieName = movie.trim().toLowerCase();
+        //find or insert the movie in moviesInfo
+        //Boolean exsit = false;  
+        
+        if (ratings.length !=2 || ratings == null) {
+            return ;
+        }
+        if (ratings[0]>100 || ratings[0]<0 || ratings[1]>100 || ratings[1]<0) {
+            return;
+        }
+        
+        for (Movie singleMovie : moviesInfo){
+            if (singleMovie.getName().equals(newMovieName)){
+                
+                singleMovie.setCriticRating(ratings[0]);
+                singleMovie.setAudienceRating(ratings[1]);   
+                //exsit = true;  
+                return;
+            }
+        }
+        Movie targetMovie = new Movie(newMovieName, ratings[0], ratings[1]); 
+        moviesInfo.add(targetMovie);       
+    }
+    
+    /**
+     * Given an actor, return the list of all movies. 
+     * If the actor is non-existent in the database, this method should return an empty list.
+     */
+    public ArrayList <String> selectWhereActorIs (String actor, ArrayList <Actor> actorsInfo){
+        String actorName = actor.toLowerCase().trim();   // make sure the name is not case-sensitivie     
+        ArrayList<String> actorMovies = new ArrayList<String>();
+        for (Actor targetActor:actorsInfo){
+            if (targetActor.getName().equals(actorName)){ // == is wrong
+                actorMovies = targetActor.getMoviesCast();
+            }
+        }
+//         Actor findActor = actorsInfo.stream().filter(actor -> actor.getName().toLowerCase().trim() == actorName)
+//             .findFirst()
+//             .orElse(null); // try to find the actor in db, .stream() makes it an array
+        //if (findActor == null) return new ArrayList<String>();
+        return actorMovies;
+    }
+    
+    /**
+     * Given a movie, return the list of all actors in that movie.
+     * If the movie is non-existent, this method should return an empty list
+     */
+    public ArrayList <String> selectWhereMovieIs (String movie, ArrayList <Actor> actorsInfo){
+        String movieName = movie.toLowerCase().trim();
+        ArrayList<String> actorsList = new ArrayList<String>();
+        for (Actor actor:actorsInfo){
+            if (actor.getMoviesCast().contains(movieName)){
+                actorsList.add(actor.getName());  
+            }
+        }
+        return actorsList;
+
+    }
+    
+    /**
+     * Based on the comparison argument and the targeted rating argument, 
+     * return a list of movies that satisfy an inequality or equality.
+     */
+    public ArrayList <String> selectWhereRatingIs (char comparison, int targetRating, boolean isCritic, ArrayList <Movie> moviesInfo){
+        ArrayList<String> movies = new ArrayList<String>(); //default is an empty list
+        double rating ;
+ 
+        if (isCritic){
+            if (comparison == '>'){
+                for (Movie movie: moviesInfo){
+                    if (movie.getCriticRating() > targetRating) movies.add(movie.getName());
+                }  
+            }
+            if (comparison == '<'){
+                for (Movie movie: moviesInfo){
+                    if (movie.getCriticRating() < targetRating) movies.add(movie.getName());
+                }     
+            }
+            if (comparison == '='){
+                for (Movie movie: moviesInfo){
+                    if (movie.getCriticRating() == targetRating) movies.add(movie.getName());
+                }  
+                
+            } 
+        } else {
+            if (comparison == '>'){
+                for (Movie movie: moviesInfo){
+                    if (movie.getAudienceRating() > targetRating) movies.add(movie.getName());
+                }  
+            }
+            if (comparison == '<'){
+                for (Movie movie: moviesInfo){
+                    if (movie.getAudienceRating() < targetRating) movies.add(movie.getName());
+                }     
+            }
+            if (comparison == '='){
+                for (Movie movie: moviesInfo){
+                    if (movie.getAudienceRating() == targetRating) movies.add(movie.getName());
+                } 
+            }
+        }
+     return movies  ;     
+    }
+    
+    /**
+     * Return a list of all actors that the given actor has ever worked with in any movie except the actor herself/himself.
+     */
+    public ArrayList <String> getCoActors (String actor, ArrayList <Actor> actorsInfo){
+        ArrayList <String> actorMovies = selectWhereActorIs(actor, actorsInfo);
+        ArrayList<String> coActors = new ArrayList <String>();
+        for (String movie : actorMovies){
+            ArrayList <String> actors = selectWhereMovieIs(movie, actorsInfo);
+            for (String coactor : actors){
+                if(!coactor.equals(actor.toLowerCase().trim())){  //modified after testing
+                    coActors.add(coactor);
+                }
+            }
+        }
+        return coActors;   
+    }
+    
+    
+    /**
+     * Return a list of movie names where both actors were cast. 
+     * In cases where the two actors have never worked together, this method returns an empty list.
+     */
+    public ArrayList <String> getCommonMovie (String actor1, String actor2, ArrayList <Actor> actorsInfo){
+        ArrayList<String> commonMovies = new ArrayList <String>();
+        ArrayList <String> actor1Movies = selectWhereActorIs (actor1, actorsInfo);
+        ArrayList <String> actor2Movies = selectWhereActorIs (actor2, actorsInfo);
+        if (actor1Movies == null || actor2Movies == null) return commonMovies;
+        
+        for (String movie : actor1Movies){
+            if (actor2Movies.contains(movie)){
+                commonMovies.add(movie);
+            }
+        }
+        return commonMovies;
+    }
+    
+    
+    /**
+     * Returnsa list of movie names that both critics and the audience have rated above 85 (>= 85).
+     */
+    public ArrayList <String> goodMovies (ArrayList <Movie> moviesInfo){
+        ArrayList<String> goodMovies = new ArrayList <String>();
+        for (Movie movie : moviesInfo){
+            if (movie.getCriticRating() >= 85 && movie.getAudienceRating() >= 85){
+                goodMovies.add(movie.getName());
+            }
+        }
+        return goodMovies;
+    }
+    
+    /**
+     * Given a pair of movies, this method returns a list of actors that acted in both movies. 
+     * In cases where the movies have no actors in common, it returns an empty list.
+     */
+    public ArrayList <String> getCommonActors (String movie1, String movie2, ArrayList <Actor> actorsInfo){
+        ArrayList<String> commonActors = new ArrayList <String>(); //initialize as empty list
+        ArrayList <String> movie1Actors= selectWhereMovieIs (movie1, actorsInfo); //already know their datatypes 
+        ArrayList <String> movie2Actors= selectWhereMovieIs (movie2, actorsInfo);
+        if (movie1Actors==null || movie2Actors==null) return  commonActors;
+        for (String actor : movie1Actors){
+            if (movie2Actors.contains(actor)) {
+                commonActors.add(actor);
+            }
+        }
+        return  commonActors;
+    }
+    
+    
+    /**
+     * Given the moviesInfo DB, this static method returns the mean value of the critics’ ratings and the audience ratings.
+     */
+    public static double [] getMean (ArrayList <Movie> moviesInfo){
+        double[] mean = new double[2] ;
+        double criticRatings = 0 ;
+        double audienceRatings = 0;
+        int count = moviesInfo.size();
+        for (Movie movie : moviesInfo){
+            criticRatings += movie.getCriticRating();
+            audienceRatings += movie.getAudienceRating();
+            // count += 1 , since we used .size(), no need to do this here.
+        }
+        mean[0]=  criticRatings / count ;
+        mean[1]= audienceRatings / count ;    
+        return mean;
+    }
+    
 }
--- src/MovieTriviaTest.java
+++ src/MovieTriviaTest.java
@@ -48,6 +48,20 @@ class MovieTriviaTest {
 		assertEquals("testmovie1", movieDB.getActorsInfo().get(movieDB.getActorsInfo().size() - 1).getMoviesCast().get(0));
 		
 		// TODO add additional test case scenarios
+        mt.insertActor("meryl streep", new String [] {"the bridge"}, movieDB.getActorsInfo());
+		assertEquals(7, movieDB.getActorsInfo().size());	
+		assertEquals("the bridge", movieDB.getActorsInfo().get(0).getMoviesCast().get(3));
+		
+        mt.insertActor("  RobiN Williams", new String [] {"DeaD poet society", "the chorus"}, movieDB.getActorsInfo());
+		assertEquals(7, movieDB.getActorsInfo().size());	
+		assertEquals("dead poet society", movieDB.getActorsInfo().get(4).getMoviesCast().get(1));
+		
+        mt.insertActor("Brad PITT", new String [] {"   Inglourious Basterds ","ONCE Upon A Time in Hollywood     "}, movieDB.getActorsInfo());
+		assertEquals(7, movieDB.getActorsInfo().size());	
+		assertEquals("brad pitt", movieDB.getActorsInfo().get(5).getName());
+		assertEquals(4, movieDB.getActorsInfo().get(5).getMoviesCast().size());
+		assertEquals("inglourious basterds", movieDB.getActorsInfo().get(5).getMoviesCast().get(2));
+		
 		
 	}
 	
@@ -59,7 +73,29 @@ class MovieTriviaTest {
 		assertEquals(79, movieDB.getMoviesInfo().get(movieDB.getMoviesInfo().size() - 1).getCriticRating());
 		assertEquals(80, movieDB.getMoviesInfo().get(movieDB.getMoviesInfo().size() - 1).getAudienceRating());
 		
-		// TODO add additional test case scenarios
+// 		TODO add additional test case scenarios
+        mt.insertRating(" Arrival", new int [] {80, 95}, movieDB.getMoviesInfo());
+		assertEquals(8, movieDB.getMoviesInfo().size());	
+		assertEquals(80, movieDB.getMoviesInfo().get(1).getCriticRating());
+		assertEquals(95, movieDB.getMoviesInfo().get(1).getAudienceRating());
+		
+        mt.insertRating(" jawS ", new int[] {90, 90}, movieDB.getMoviesInfo());
+		assertEquals(8, movieDB.getMoviesInfo().size());	
+		assertEquals(90, movieDB.getMoviesInfo().get(2).getCriticRating());
+		assertEquals(90, movieDB.getMoviesInfo().get(2).getAudienceRating());
+		
+        mt.insertRating("doubt", new int [] {}, movieDB.getMoviesInfo());
+		assertEquals(8, movieDB.getMoviesInfo().size());	
+		assertEquals("doubt", movieDB.getMoviesInfo().get(0).getName());
+		assertEquals(79, movieDB.getMoviesInfo().get(0).getCriticRating());
+		assertEquals(78, movieDB.getMoviesInfo().get(0).getAudienceRating());
+		
+        mt.insertRating(" doubt", new int [] {101,80}, movieDB.getMoviesInfo());
+		assertEquals(8, movieDB.getMoviesInfo().size());	
+		assertEquals("doubt", movieDB.getMoviesInfo().get(0).getName());
+		assertEquals(79, movieDB.getMoviesInfo().get(0).getCriticRating());
+		assertEquals(78, movieDB.getMoviesInfo().get(0).getAudienceRating());
+		
 	}
 	
 	@Test
@@ -68,6 +104,16 @@ class MovieTriviaTest {
 		assertEquals("doubt", mt.selectWhereActorIs("meryl streep", movieDB.getActorsInfo()).get(0));
 		
 		// TODO add additional test case scenarios
+        assertEquals(3, mt.selectWhereActorIs("Meryl stREep", movieDB.getActorsInfo()).size());
+		assertEquals("the post", mt.selectWhereActorIs("Meryl stREep", movieDB.getActorsInfo()).get(2));
+		
+        assertEquals(4, mt.selectWhereActorIs("Amy Adams", movieDB.getActorsInfo()).size());
+		assertEquals("leap year", mt.selectWhereActorIs("Amy Adams", movieDB.getActorsInfo()).get(1));
+		
+        assertEquals(0, mt.selectWhereActorIs("brandon krakowsky", movieDB.getActorsInfo()).size());
+        
+		assertEquals(0, mt.selectWhereActorIs("luna", movieDB.getActorsInfo()).size());
+		
 	}
 	
 	@Test
@@ -77,6 +123,16 @@ class MovieTriviaTest {
 		assertEquals(true, mt.selectWhereMovieIs("doubt", movieDB.getActorsInfo()).contains("amy adams"));
 		
 		// TODO add additional test case scenarios
+        assertEquals(2, mt.selectWhereMovieIs("  Doubt", movieDB.getActorsInfo()).size());
+		assertEquals(true, mt.selectWhereMovieIs("  Doubt", movieDB.getActorsInfo()).contains("meryl streep"));
+		assertEquals(true, mt.selectWhereMovieIs("  Doubt", movieDB.getActorsInfo()).contains("amy adams"));
+		
+        assertEquals(2, mt.selectWhereMovieIs("The Post", movieDB.getActorsInfo()).size());
+		assertEquals(true, mt.selectWhereMovieIs("The Post", movieDB.getActorsInfo()).contains("meryl streep"));
+		assertEquals(true, mt.selectWhereMovieIs("The Post", movieDB.getActorsInfo()).contains("tom hanks"));
+		
+        assertEquals(0, mt.selectWhereMovieIs("titanic", movieDB.getActorsInfo()).size());
+		
 	}
 	
 	@Test
@@ -86,6 +142,11 @@ class MovieTriviaTest {
 		assertEquals(2, mt.selectWhereRatingIs('<', 30, true, movieDB.getMoviesInfo()).size());
 		
 		// TODO add additional test case scenarios
+        assertEquals(1, mt.selectWhereRatingIs('=', 29, true, movieDB.getMoviesInfo()).size());
+		assertEquals(5, mt.selectWhereRatingIs('>', 70, false, movieDB.getMoviesInfo()).size());
+		assertEquals(4, mt.selectWhereRatingIs('<', 90, true, movieDB.getMoviesInfo()).size());
+        assertEquals(0, mt.selectWhereRatingIs('?', 29, true, movieDB.getMoviesInfo()).size());
+		
 	}
 	
 	@Test
@@ -95,6 +156,16 @@ class MovieTriviaTest {
 		assertTrue(mt.getCoActors("meryl streep", movieDB.getActorsInfo()).contains("amy adams"));
 		
 		// TODO add additional test case scenarios
+        assertEquals(2, mt.getCoActors("  Meryl streeP", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCoActors("  Meryl streeP", movieDB.getActorsInfo()).contains("tom hanks"));
+		assertTrue(mt.getCoActors("  Meryl streeP", movieDB.getActorsInfo()).contains("amy adams"));
+
+        assertEquals(1, mt.getCoActors("Amy Adams", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCoActors("Amy Adams", movieDB.getActorsInfo()).contains("meryl streep"));
+
+        assertEquals(0, mt.getCoActors("robin williams", movieDB.getActorsInfo()).size());
+        assertEquals(0, mt.getCoActors("kate winslate", movieDB.getActorsInfo()).size());
+
 	}
 	
 	@Test
@@ -103,6 +174,19 @@ class MovieTriviaTest {
 		assertTrue(mt.getCommonMovie("meryl streep", "tom hanks", movieDB.getActorsInfo()).contains("the post"));
 		
 		// TODO add additional test case scenarios
+        
+        assertEquals(1, mt.getCommonMovie("   MeryL streep", "tom Hanks  ", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCommonMovie("   MeryL streep", "tom Hanks  ", movieDB.getActorsInfo()).contains("the post"));
+		
+        assertEquals(1, mt.getCommonMovie("meryl streep", "AMY ADAms", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCommonMovie("meryl streep", "AMY ADAms", movieDB.getActorsInfo()).contains("doubt"));
+		
+        assertEquals(1, mt.getCommonMovie("robin williams", "robin williams", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCommonMovie("robin williams", "robin williams", movieDB.getActorsInfo()).contains("popeye"));
+        
+        assertEquals(0, mt.getCommonMovie("robin williams", "tom hanks", movieDB.getActorsInfo()).size());
+        assertEquals(0, mt.getCommonMovie("leonardo", "tom hanks", movieDB.getActorsInfo()).size());
+
 	}
 	
 	@Test
@@ -111,6 +195,11 @@ class MovieTriviaTest {
 		assertTrue(mt.goodMovies(movieDB.getMoviesInfo()).contains("jaws"));
 		
 		// TODO add additional test case scenarios
+        assertTrue(mt.goodMovies(movieDB.getMoviesInfo()).contains("rocky ii"));
+        assertTrue(mt.goodMovies(movieDB.getMoviesInfo()).contains("et"));
+        assertFalse(mt.goodMovies(movieDB.getMoviesInfo()).contains("Popeye"));
+        assertFalse(mt.goodMovies(movieDB.getMoviesInfo()).contains("arrival"));
+        assertFalse(mt.goodMovies(movieDB.getMoviesInfo()).contains("seven"));
 	}
 	
 	@Test
@@ -119,12 +208,25 @@ class MovieTriviaTest {
 		assertTrue(mt.getCommonActors("doubt", "the post", movieDB.getActorsInfo()).contains("meryl streep"));
 		
 		// TODO add additional test case scenarios
+        assertEquals(1, mt.getCommonActors("The Post", "  catch Me if You Can", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCommonActors("The Post", "catch Me if You Can", movieDB.getActorsInfo()).contains("tom hanks"));
+
+        assertEquals(0, mt.getCommonActors("doubt", "popeye", movieDB.getActorsInfo()).size());
+        
+        assertEquals(0, mt.getCommonActors("titanic", "popeye", movieDB.getActorsInfo()).size());
+        
+        assertEquals(2, mt.getCommonActors("the post", "the post", movieDB.getActorsInfo()).size());
+		assertTrue(mt.getCommonActors("the post", "the post", movieDB.getActorsInfo()).contains("meryl streep"));
+        assertTrue(mt.getCommonActors("the post", "the post", movieDB.getActorsInfo()).contains("tom hanks"));
+
 	}
 	
 	@Test
 	void testGetMean () {
-		fail();
-		
+		//fail();
 		// TODO add ALL test case scenarios!
+        assertEquals(2, mt.getMean(movieDB.getMoviesInfo()).length);
+        assertEquals(67.85714285714286, mt.getMean(movieDB.getMoviesInfo())[0]);
+        assertEquals(65.71428571428571, mt.getMean(movieDB.getMoviesInfo())[1]);
 	}
 }
